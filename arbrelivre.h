d918ee3e023f9767412171e51026660817cfbcd7
BastianCorlay <96916556+BastianCorlay@users.noreply.github.com>
2022-01-02 11:37:52 +0100

    Create arbrelivre.h

---
+++ arbrelivre.h
@@ -0,0 +1,28 @@
+#ifndef ARBRELIVRE_H_INCLUDED
+#définir ARBRELIVRE_H_INCLUDED
+/*
+struct Noeud
+{
+    int valeur ;
+    char nomexistant[100];
+    struct Noeud* gauche ;
+    struct Noeud* droit ;
+}TNoeud;
+
+TArbre NouvelArbreVide( void );
+
+void Ajouter( TArbre* arbre , int val );
+
+int chercher(TArbre arbre,int v);
+
+void AfficherValeur( TArbre arbre );
+
+void AfficherNom( TArbre arbre );
+
+int NombreElements( TArbre arbre );
+
+int Hauteur( TArbre arbre );
+
+int SupElement(TArbre* arbre, int info);
+*/
+#endif  // ARBRELIVRE_H_INCLUDED
